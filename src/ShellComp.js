981e7ff29bac1cca542e42feb6176ff272aef504
soundsXsweet <57301173+soundsXsweet@users.noreply.github.com>
2021-03-18 21:21:52 -0500

    url-safe-swim refactored requests

--- src/ShellComp.js
+++ src/ShellComp.js
@@ -32,14 +32,13 @@ export default function ShellComp() {
 
     function virusTotalScanSearch(input) {
         const apiKey = '142513a6f4669a85806410b9d39f46aeb6fac11620dac13c1131fd64320543b1';
-       const searchUrl = 'https://www.virustotal.com/vtapi/v2/url/report';
+       const searchUrl = 'https://www.virustotal.com/vtapi/v2/url/report?apikey=' + apiKey + '&resource=' + input;
        // const scanUrl = 'https://www.virustotal.com/vtapi/v2/url/scan';
        const searchHttp = new XMLHttpRequest();
        // const scanHttp = new XMLHttpRequest();
-        let searchParams = {'apikey': apiKey, 'resource': input}
+       // let searchParams = {'apikey': apiKey, 'resource': input}
         searchHttp.open("GET", searchUrl);
-        searchHttp.setRequestHeader("Origin", 'google.com');
-        searchHttp.send(searchParams);
+        searchHttp.send();
         searchHttp.onreadystatechange=function(){
                      if(this.readyState===4 && this.status===200){
                          console.log(searchHttp.responseText)
