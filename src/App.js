eafcde6157991b81f65b1aace1021a22f26b79b2
soundsXsweet <57301173+soundsXsweet@users.noreply.github.com>
2021-03-19 20:17:18 -0500

    url-safe-swim finally figured out how to pass variables through localstorage, added method to clear and reload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ShellComp from './ShellComp';
 
 function App() {
+
+  
   return (
     <>
     <ShellComp />
--- src/ShellComp.js
+++ src/ShellComp.js
@@ -1,9 +1,9 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 
 export default function ShellComp() {
-    
-    const [scans, setScans] = useState(null);
+    //const [localStorage, setLocalStorage] = useState({});
     const urlSearch = useRef();
+
     function searchHandler(evt) {
         const searchTerm = urlSearch.current.value;
         if (searchTerm === '') return;
@@ -11,25 +11,56 @@ export default function ShellComp() {
         urlSearch.current.value = null;
     }
 
+    function resultsHandler() {
+        var temp = JSON.parse(localStorage.getItem('response'));
+        if (temp) {
+        temp = temp.substring(temp.indexOf("scans") + 7);
+        temp = temp.replace(/{/g, '');
+        var tempArr = temp.split('},');
+        tempArr.forEach(element => {
+            document.getElementById('res-div').innerHTML += '<p>' + element + '</p>';
+        });
+        }
+        var temp2 = JSON.parse(localStorage.getItem('responseBackup'));
+        if (temp2) {
+        temp2 = temp2.substring(temp2.indexOf("scans") + 7);
+        temp2 = temp2.replace(/{/g, '');
+        var tempArr2 = temp2.split('},');
+        tempArr2.forEach(element => {
+            document.getElementById('res-div').innerHTML += '<p>' + element + '</p>';
+        });
+    }
+    }
+
     function clearHandler() {
+        localStorage.clear();
         window.location.reload(true);
     }
 
     function virusTotalScanSearch(input) {
-        let tempArr = [];
         const apiKey = '142513a6f4669a85806410b9d39f46aeb6fac11620dac13c1131fd64320543b1';
-       const searchUrl = 'https://cors-anywhere.herokuapp.com/https://www.virustotal.com/vtapi/v2/url/report?apikey=' + apiKey + '&resource=' + input;
+       const searchUrl = 'https://www.virustotal.com/vtapi/v2/url/report?apikey=' + apiKey + '&resource=' + input;
+       const backupSearchUrl = 'https://cors-anywhere.herokuapp.com' + searchUrl;
        const searchHttp = new XMLHttpRequest();
+       const backupSearchHttp = new XMLHttpRequest();
         searchHttp.open("GET", searchUrl);
+        backupSearchHttp.open("GET", backupSearchUrl);
         searchHttp.setRequestHeader('Origin', 'https://www.virustotal.com');
+        backupSearchHttp.setRequestHeader('Origin', 'https://www.virustotal.com');
         searchHttp.send();
         searchHttp.onreadystatechange=function(){
                      if(this.readyState===4 && this.status===200){
-                         const response = JSON.parse(searchHttp.responseText);
-                         setScans(response.scans);
+                         const response = JSON.stringify(searchHttp.response);
+                         localStorage.setItem('response', response);                         
                      }
     }
-    return tempArr;
+    backupSearchHttp.send();
+        backupSearchHttp.onreadystatechange=function(){
+                     if(this.readyState===4 && this.status===200){
+                         const backupResponse = JSON.parse(searchHttp.responseText);
+                         localStorage.setItem('backupResponse', backupResponse);
+                     }
+    }
 }
 
     return (
@@ -37,12 +68,10 @@ export default function ShellComp() {
         <div>
             <input ref={urlSearch} type="text" />
             <button onClick={searchHandler}>Check URL</button>
+            <button onClick={resultsHandler}>View Results</button>
             <button onClick={clearHandler}>Clear Results</button>
         </div>
-        <div>
-            <ul>
-                {this.state.scans.map(s => (<li>{s}</li>))}
-            </ul>
+        <div id='res-div'>
         </div>
         </>
     )
